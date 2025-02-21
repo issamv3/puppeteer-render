1c71a849062e40326e980ced75c46f4cd707e7ef
issamv3 <gbot2613@gmail.com>
2025-02-21 22:09:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ async function getDownloadLinkAndSize(url) {
     });
 
     const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+      if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
     await page.setExtraHTTPHeaders({
       'authority': 'www.mediafire.com',
       'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
